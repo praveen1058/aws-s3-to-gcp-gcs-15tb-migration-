840baf2d2745e9b70f7c60830c8f1f429a4fb651
praveen1058 <74782423+praveen1058@users.noreply.github.com>
2026-08-10 17:51:37 +0530

    Create provider.tf

---
+++ terraform/gcp/provider.tf
@@ -0,0 +1,17 @@
+```hcl
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+```
